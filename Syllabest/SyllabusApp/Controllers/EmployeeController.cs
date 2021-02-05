9403d84ab811c51da5f97b539cd247961b1d91ff
Stacy Berndt <stacyberndt@gmail.com>
2021-02-04 21:45:43 -0500

    created an Employee Model and an Employee Controller, began to create methods for the login -Stacy

---
+++ Syllabest/SyllabusApp/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SyllabusApp.Models;
+
+namespace SyllabusApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login() //began to create a login method
+        {
+            return View();
+        }
+        public ActionResult Verify(Employee acc) //began a method to verify the login information,just need to populate our username/password in database
+        {
+            return View();
+        }
+    }
+}
