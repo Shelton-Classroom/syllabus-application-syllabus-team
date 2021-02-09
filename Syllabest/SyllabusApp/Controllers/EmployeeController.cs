01055115c43600a6c4b428895f6616e9913ab98f
c0linjb <70547034+c0linjb@users.noreply.github.com>
2021-02-08 19:31:06 -0500

    Added Link

--- Syllabest/SyllabusApp/Controllers/EmployeeController.cs
+++ Syllabest/SyllabusApp/Controllers/EmployeeController.cs
@@ -10,25 +10,20 @@ namespace SyllabusApp.Controllers
 {
     public class EmployeeController : Controller
     {
-<<<<<<< HEAD
-       // SqlConnection con = new SqlConnection();    //this connects our commands to our database
-       // SqlCommand com = new SqlCommand();
-       // SqlDataReader sdr;  //this class reads the data 
+
 
         // GET: Account
         [HttpGet]
-        public ActionResult Login() //began to create a login method, need to add a view for it
+        public ActionResult Login()
         {
             return View();
         }
 
         
             
-=======
 
-        // GET: Account
-        [HttpGet]
+      
         
->>>>>>> c89b7b2beb337bf3b4eddb164c7d3a0a272b4a51
+
     }
 }
