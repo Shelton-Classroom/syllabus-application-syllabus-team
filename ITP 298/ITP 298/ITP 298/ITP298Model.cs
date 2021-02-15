11e33dd5d9b143980649d146f59d9b5b7f125e4e
sberndt8089 <70170961+sberndt8089@users.noreply.github.com>
2021-02-14 19:45:06 -0500

    created new MVC app that has a login/registration

---
+++ ITP 298/ITP 298/ITP 298/ITP298Model.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Web;
+
+
+namespace ITP_298
+{
+    [MetadataType(typeof(CategoryMetadata))]
+    public partial class Employee
+    {
+        private sealed class CategoryMetadata
+        {
+            [Display(Name = "Employee ID")]
+            public int EmployeeId { get; set; }
+
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [Display(Name = "Employee Status Id")]
+            public int EmployeeStatusId { get; set; }
+
+            [Display(Name = "Phone Number")]
+            public int PhoneNum { get; set; }
+
+            [Display(Name = "Office Hours")]
+            public DateTime OfficeHours { get; set; }
+
+
+        }
+    }
+}
