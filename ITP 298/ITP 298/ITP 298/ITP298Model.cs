907182128129f56223e88dfe5aee8ac67e268ef7
c0linjb <70547034+c0linjb@users.noreply.github.com>
2021-02-27 20:55:25 -0500

    Added Deo's Metadata to course

--- ITP 298/ITP 298/ITP 298/ITP298Model.cs
+++ ITP 298/ITP 298/ITP 298/ITP298Model.cs
@@ -10,11 +10,11 @@ using System.Web;
 
 namespace ITP_298
 {
-    [MetadataType(typeof(CategoryMetadata))]
+    [MetadataType(typeof(EmployeeMetadata))]
     public partial class Employee
     {
         public string Name => $"{FirstName} {LastName}";
-        private sealed class CategoryMetadata
+        private sealed class EmployeeMetadata
         {
             [Display(Name = "Employee ID")]
             public int EmployeeId { get; set; }
@@ -23,7 +23,7 @@ namespace ITP_298
             public string FirstName { get; set; }
 
             [Display(Name = "Last Name")]
-            public string LastName { get; set; }          
+            public string LastName { get; set; }
 
             [Display(Name = "Employee Status")]
             public int EmployeeStatusId { get; set; }
@@ -36,6 +36,48 @@ namespace ITP_298
 
             [Display(Name = "Email")]
             public string LoginUserId { get; set; }
+
+
+        }
+    }
+
+    [MetadataType(typeof(AspNetUserMetadata))]
+    public partial class AspNetUser
+    {
+        private sealed class AspNetUserMetadata
+        {
+
+        }
+
+    }
+
+    [MetadataType(typeof(CourseMetadata))]
+
+    public partial class Course
+    {
+
+        public sealed class CourseMetadata
+        {
+            //[Display(Name = "Title")]
+            //public string Title { get; set; }
+
+            //[Display(Name = "Credits")]
+            //public int Credit { get; set; }
+
+            //[Display(Name = "Description")]
+            //public string Description { get; set; }
+
+            //[Display(Name = "Prerequisite")]
+            //public string Preriquisite { get; set; }
+
+            [Display(Name = "Credits")]
+            public int Credit { get; set; }
+
+            [Display(Name = "Department Code")]
+            public string DepartmentCode { get; set; }
+
+            [Display(Name = "Course Number")]
+            public int CourseNumber { get; set; }
 
 
         }
