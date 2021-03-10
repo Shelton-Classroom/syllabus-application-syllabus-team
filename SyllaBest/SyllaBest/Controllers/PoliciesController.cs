6ff8235a4464a88dad540964b92982cd65ab0dd7
c0linjb <70547034+c0linjb@users.noreply.github.com>
2021-03-09 21:42:06 -0500

    All Working Except ASP Authentication

---
+++ SyllaBest/SyllaBest/Controllers/PoliciesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ITP_298;
+
+namespace Application.Controllers
+{
+    public class PoliciesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Policies
+        public ActionResult Index()
+        {
+            var policies = db.Policies.Include(p => p.Group).Include(p => p.Section);
+            return View(policies.ToList());
+        }
+
+        // GET: Policies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Policy policy = db.Policies.Find(id);
+            if (policy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(policy);
+        }
+
+        // GET: Policies/Create
+        public ActionResult Create()
+        {
+            ViewBag.GroupId = new SelectList(db.Groups, "GroupId", "Name");
+            ViewBag.SectionId = new SelectList(db.Sections, "SectionId", "Mode");
+            return View();
+        }
+
+        // POST: Policies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PolicyId,Title,Description,SectionId,GroupId")] Policy policy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Policies.Add(policy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GroupId = new SelectList(db.Groups, "GroupId", "Name", policy.GroupId);
+            ViewBag.SectionId = new SelectList(db.Sections, "SectionId", "Mode", policy.SectionId);
+            return View(policy);
+        }
+
+        // GET: Policies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Policy policy = db.Policies.Find(id);
+            if (policy == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GroupId = new SelectList(db.Groups, "GroupId", "Name", policy.GroupId);
+            ViewBag.SectionId = new SelectList(db.Sections, "SectionId", "Mode", policy.SectionId);
+            return View(policy);
+        }
+
+        // POST: Policies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PolicyId,Title,Description,SectionId,GroupId")] Policy policy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(policy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GroupId = new SelectList(db.Groups, "GroupId", "Name", policy.GroupId);
+            ViewBag.SectionId = new SelectList(db.Sections, "SectionId", "Mode", policy.SectionId);
+            return View(policy);
+        }
+
+        // GET: Policies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Policy policy = db.Policies.Find(id);
+            if (policy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(policy);
+        }
+
+        // POST: Policies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Policy policy = db.Policies.Find(id);
+            db.Policies.Remove(policy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SyllaBest/SyllaBest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SyllaBest.Startup))]
+namespace SyllaBest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
